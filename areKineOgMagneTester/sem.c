3659f6de12f602b992ffdceb69978ff73a17c249
Are Tømmerås <are@tommeras.net>
2022-03-17 15:43:06 +0100

    ny fil

---
+++ areKineOgMagneTester/sem.c
@@ -0,0 +1,37 @@
+#include "sem.h"
+
+int value;
+int pshared;
+int ret;
+
+struct SEM{
+    int value;
+    int pshared;
+    int ret;
+}
+
+sem_init(int initVal) {
+    SEM new;
+    new.value = initVal;
+    new.pshared = 0;
+
+    return new; 
+};
+
+
+int sem_del(SEM *sem){
+
+};
+
+void P(SEM *sem){
+    sem->value = sem->value-1;
+    
+
+};
+
+void V(SEM *sem){
+
+    sem->value = sem->value+1;
+
+
+};
