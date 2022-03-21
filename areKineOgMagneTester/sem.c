e26eaecab48fbdf9dcf6585a226d9b7337aa0b50
Are Tømmerås <are@tommeras.net>
2022-03-21 13:40:30 +0100

    noen nye funksjoner fungerer

--- areKineOgMagneTester/sem.c
+++ areKineOgMagneTester/sem.c
@@ -1,21 +1,25 @@
 #include "sem.h"
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int value;
 int pshared;
-int ret;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
-struct SEM{
+typedef struct SEM{
     int value;
-    int pshared;
-    int ret;
-}
+    pthread_cond_t cond;
+    pthread_mutex_t mutex;
+}SEM;
 
-Tem_init(int initVal) {
-    SEM new;
-    new.value = initVal;
-    new.pshared = 0;
+SEM *sem_init(int initVal) {
+    SEM *sem = malloc(sizeof(struct SEM));
+    //SEM *new;
+    sem -> value = initVal;
+    //(*new).value = 1;
 
-    return new; 
+    return sem; 
 };
 
 
@@ -24,7 +28,20 @@ int sem_del(SEM *sem){
 };
 
 void P(SEM *sem){
-    sem->value = sem->value-1;
+    if(sem->value==0){
+        int i = pthread_cond_wait(&sem->cond, &sem->mutex);
+        if(i=0){
+            printf("i=0\n");
+        }
+        else{
+            printf("i=/=0\n");
+        }
+
+    }
+    else{
+        sem->value = sem->value-1;
+    }
+    
     
 
 };
@@ -35,3 +52,17 @@ void V(SEM *sem){
 
 
 };
+
+
+void main(){
+    SEM *sem = sem_init(2);
+    printf("%d\n", sem->value);
+    P(sem);
+    printf("%d\n", sem->value);
+    P(sem);
+    printf("%d\n", sem->value);
+    P(sem);
+    printf("%d\n", sem->value);
+
+
+}
