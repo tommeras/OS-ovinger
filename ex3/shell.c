25a47fa4365d3c43eec276e644dc608d981576dd
Kine Kai Moe Holthe <kkholthe@stud.ntnu.no>
2022-04-15 19:49:27 +0200

    Finish 3.2

--- ex3/shell.c
+++ ex3/shell.c
@@ -1,91 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
 
+#define	MAX_SIZE_CMD	256
 #define PATH_MAX 1024
-#define LSH_TOK_BUFSIZE 64
+#define	MAX_SIZE_ARG	16
 
+char cwd[PATH_MAX];
+char cmd[MAX_SIZE_CMD];		
+char *argv[MAX_SIZE_ARG];			
+pid_t pid;										
+char i;												
 
-char* takeInput () {
-    char *buffer;
-    size_t size = 32;
-    size_t characters;
-
-    char cwd[PATH_MAX];
+void takeInput(){
+    
     if (getcwd(cwd, sizeof(cwd)) == NULL) {
         perror("getcwd() error");
-        return "Error";
     } 
 
-    buffer = (char *)malloc(size * sizeof(char));
-    if( buffer == NULL)
-    {
-        perror("Unable to allocate buffer");
-        exit(1);
-    }
-
     printf("%s: ", cwd);
-    characters = getline(&buffer,&size,stdin);
-    
-    printf("%s", buffer);
-    return buffer;
+	fgets(cmd, MAX_SIZE_CMD, stdin);
+
+	if ((strlen(cmd) > 0) && (cmd[strlen (cmd) - 1] == '\n'))
+        	cmd[strlen (cmd) - 1] = '\0';
 }
 
-char **parseInput(char *line) {
+void parseInput(){
 
-    int bufsize = LSH_TOK_BUFSIZE, position = 0;
+	char *token;
+	i = 0;
+	token = strtok(cmd, " ");
+	while(token != NULL){
+		argv[i] = token;
+		i++;
+		token = strtok(NULL, " ");
+	}
+}
 
-    char **tokens = malloc(bufsize * sizeof(char*));
+void executeCD(char *args[]){
+    if (args[1] == NULL) {
+		chdir(getenv("HOME")); 
+    }
+	else{ 
+		if (chdir(args[1]) == -1) {
+			printf(" %s: no such directory\n", args[1]);
+		}
+	}
+}
 
-    char *token;
-    token = strtok(line, " ");
+int startShell(){
+	while(1){
+		takeInput();
+        parseInput();
 
-    while (token != NULL) {
-        tokens[position] = token;
-        position++;
-
-        if (position >= bufsize) {
-            bufsize += LSH_TOK_BUFSIZE;
-            tokens = realloc(tokens, bufsize * sizeof(char*));
-            if (!tokens) {
-                fprintf(stderr, "lsh: allocation error\n");
-                exit(EXIT_FAILURE);
-            }
+        if(!strcmp("cd", cmd)) {
+            executeCD(argv);
+            continue;
         }
 
-        token = strtok(NULL, " ");
-    }    
-
-    pid_t pid = fork();
-
-    if(pid == 0) {
-        execv(tokens[0], tokens);
-    }
-
-    int status;
+		pid = fork();
+		if(pid == 0){
+            execvp(argv[0], argv);
+		}
+      
+        int status;
      
-    waitpid(pid, &status, 0);
+            waitpid(pid, &status, 0);
 
-    if ( WIFEXITED(status) ) {
-        int exit_status = WEXITSTATUS(status);       
-        printf("Exit status [%s] = %d\n", tokens[0], exit_status);
-    }
-
-    return tokens;
+            if ( WIFEXITED(status) ) {
+                int exit_status = WEXITSTATUS(status);       
+                printf("Exit status [%s] = %d\n", cmd, exit_status);
+            }
+      
+	}
 }
 
+int main(){
+	
+	startShell();
 
-int main () {
-   
-
-    do{
-        char *input = takeInput();
-        char** arguments = parseInput(input);
-    }
-
-    while (1); 
-
-     
-    
+	return 0;
 }
